c490d03fc7cc7615601d9415627f88e8a3fc99cd
lalachaimaenaciri <lala.chaimae.naciri@gmail.com>
2023-05-28 21:02:25 +0200

    optimize requirements.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
-from setuptools import find_packages, setup
+import pathlib
+from setuptools import setup
+from setuptools import find_packages
+from pkg_resources import parse_requirements
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in parse_requirements(requirements_txt)
+    ]
 
 setup(
     name='src',
     packages=find_packages(),
+    install_requires=install_requires,
     version='0.1.0',
-    description='A short description of the project.',
-    author='Your name (or your organization/company/team)',
-    license='',
+    description='The analysis of taste function of healthy controls and patients.',
+    author='Lala Chaimae Naciri (University of Cagliari)',
+    license='General Public License',
 )
